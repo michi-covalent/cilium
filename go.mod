a289a54647c15780f1a8d5266af8d7b55188eb06
Jarno Rajahalme <jarno@isovalent.com>
2021-09-21 10:52:29 +0300

    bpf: Add support for re-entering LXC egress path after L7 LB
    Allow host to tail call to lxc egress for policy enforcement after L7 LB.
    
    This requires L7 LB proxy to set the source Endpoint ID in the socket
    mark. This mark includes a new magic value that the datapath
    recognizes to subject the traffic to source LXC (policy enforcement)
    processing again. Note that this does not loop endlessly due to the L7
    LB switching from service frontend IP to backend IP.
    
    Envoy image is updated to support the new EndpointId option for the
    bpf_metadata listener filter.
    
    iptables rules are amended to avoid overwriting the new "from L7 LB"
    mark with "from host" mark.
    
    A new flag 'from_l7lb' is added to conntrack to enable redirecting
    reply packets back to L7 LB proxy.
    
    Add a new map for bpf_host to tail call into for LXC policy
    enforcement after L7 LB.
    
    Some kernels reached their eBPF complexity limit with the new code
    compiled in. Address this by making all L7 LB related code in
    bpf_lxc.c confitional on the new ENABLE_L7_LB. This allows the new
    feature to be used on kernels that allow a but more complex eBPF
    programs.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- go.mod
+++ go.mod
@@ -22,7 +22,7 @@ require (
 	github.com/cilium/deepequal-gen v0.0.0-20200406125435-ad6a9003139e
 	github.com/cilium/ebpf v0.7.0
 	github.com/cilium/ipam v0.0.0-20201106170308-4184bc4bf9d6
-	github.com/cilium/proxy v0.0.0-20210917102344-9a433903f7b8
+	github.com/cilium/proxy v0.0.0-20210921123743-5346c856ab8c
 	github.com/cilium/workerpool v1.1.0
 	github.com/containernetworking/cni v0.8.1
 	github.com/containernetworking/plugins v0.9.1
--- pkg/envoy/server.go
+++ pkg/envoy/server.go
@@ -620,7 +620,7 @@ func (s *XDSServer) deleteEndpoint(name string, wg *completion.WaitGroup, callba
 	return s.endpointMutator.Delete(EndpointTypeURL, name, []string{"127.0.0.1"}, wg, callback)
 }
 
-func getListenerFilter(isIngress bool, mayUseOriginalSourceAddr bool) *envoy_config_listener.ListenerFilter {
+func getListenerFilter(isIngress bool, mayUseOriginalSourceAddr bool, l7lb bool) *envoy_config_listener.ListenerFilter {
 	return &envoy_config_listener.ListenerFilter{
 		Name: "cilium.bpf_metadata",
 		ConfigType: &envoy_config_listener.ListenerFilter_TypedConfig{
@@ -628,6 +628,7 @@ func getListenerFilter(isIngress bool, mayUseOriginalSourceAddr bool) *envoy_con
 				IsIngress:                   isIngress,
 				MayUseOriginalSourceAddress: mayUseOriginalSourceAddr,
 				BpfRoot:                     bpf.GetMapRoot(),
+				EgressMarkSourceEndpointId:  l7lb,
 			}),
 		},
 	}
@@ -663,7 +664,7 @@ func (s *XDSServer) getListenerConf(name string, kind policy.L7ParserType, port 
 		},
 		// FilterChains: []*envoy_config_listener.FilterChain
 		ListenerFilters: []*envoy_config_listener.ListenerFilter{
-			getListenerFilter(isIngress, mayUseOriginalSourceAddr),
+			getListenerFilter(isIngress, mayUseOriginalSourceAddr, false),
 		},
 	}
 
@@ -1456,7 +1457,7 @@ func getNetworkPolicy(ep logger.EndpointUpdater, name string, policy *policy.L4P
 	ingressPolicyEnforced, egressPolicyEnforced bool) *cilium.NetworkPolicy {
 	p := &cilium.NetworkPolicy{
 		Name:             name,
-		Policy:           uint64(ep.GetIdentityLocked()),
+		EndpointId:       uint64(ep.GetID()),
 		ConntrackMapName: ep.ConntrackNameLocked(),
 	}
 
